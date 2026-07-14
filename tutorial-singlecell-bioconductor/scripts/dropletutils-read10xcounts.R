2ac913523c4ec35c753b9457e074e65912e97b01
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2026-07-14 12:07:56 +0100

    continue to visualiation of qc metrics after filtering

---
+++ tutorial-singlecell-bioconductor/scripts/dropletutils-read10xcounts.R
@@ -0,0 +1,41 @@
+#!/usr/bin/env Rscript
+
+suppressPackageStartupMessages({
+	library(optparse)
+	library(DropletUtils)
+	library(LoomExperiment)
+})
+
+option_list <- list(
+	make_option(
+		c("-s", "--samples"),
+		type = "character",
+		default = "data/",
+		help = "Directory containing the 10x Genomics files [default %default]"
+	),
+	make_option(
+		c("-o", "--output"),
+		type = "character",
+		default = "outputs/sce.loom",
+		help = "Path to the output loom file [default %default]"
+	)
+)
+
+opt <- parse_args(OptionParser(option_list = option_list))
+
+sce <- DropletUtils::read10xCounts(
+	samples = opt$samples,
+	row.names = "symbol"
+)
+
+output_dir <- dirname(opt$output)
+if (!dir.exists(output_dir)) {
+	dir.create(output_dir, recursive = TRUE)
+}
+
+scle <- as(sce, "SingleCellLoomExperiment")
+if (!file.exists(opt$output)) {
+	export(object = scle, con = opt$output, format = "loom")
+}
+
+rm(list = ls())
